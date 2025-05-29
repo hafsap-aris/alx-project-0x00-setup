09ab18c0df08cbb8868a707519abd82ca6d7c22f
yohannesm <jonmersha@gmail.com>
2025-05-29 14:27:16 +0300

    ex3

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -10,4 +10,19 @@ const Users = () => {
     </div>
   );
 }
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+
+
 export default Users;
+
