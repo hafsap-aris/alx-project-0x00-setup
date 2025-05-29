38d66b7f3e032cde6ddcfa746db54f39ed665882
yohannesm <jonmersha@gmail.com>
2025-05-29 13:49:01 +0300

    repo created again

---
+++ alx-project-0x01/pages/index.tsx
@@ -0,0 +1,12 @@
+const Home: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-7xl font-thin">
+        Wel come Page
+
+      </h1>
+    </div>
+  )
+}
+
+export default Home;
