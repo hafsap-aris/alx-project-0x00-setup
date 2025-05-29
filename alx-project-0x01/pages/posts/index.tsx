1db293c7af78c4649bdc1e6c3bccc20aed8e445b
yohannesm <jonmersha@gmail.com>
2025-05-29 14:13:34 +0300

    header and index

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,6 +1,10 @@
+import Header from "@/components/layout/Header";
+
 const Posts = () => {
   return (
     <div>
+            <Header />
+
       <h1>Posts</h1>
       <p>This is the posts page.</p>
     </div>
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,6 +1,10 @@
+import Header from "@/components/layout/Header";
+
 const Users = () => {
   return (
     <div>
+            <Header />
+
       <h1>Users</h1>
       <p>This is the users page.</p>
     </div>
